71c4ac3209605fb92d36a01819bc0b203c7eaf41
David Griffin <findgriffin@gmail.com>
2013-08-09 15:59:18 +1000

    test: started to implement unit testing for naga

---
+++ test/testnaga.py
@@ -0,0 +1,24 @@
+from naga import naga
+import unittest
+
+class TestNaga(unittest.TestCase):
+
+    def test_options(self):
+        pass 
+
+    def test_cpu(self):
+        pass
+    def test_disk(self):
+        pass
+    def test_filesystem(self):
+        pass
+    def test_load(self):
+        pass
+
+
+    def test_finish(self):
+        pass
+    def test_timecheck(self):
+        pass
+        
+
